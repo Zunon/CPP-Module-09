cd20deddfc7530cbe59d0d1bdfcbb212c94e2566
Khalifa AlMheiri <zunon@zunon.io>
2023-09-18 18:20:51 +0400

    feat(ex01): initial code for ex01 but it's incorrect

---
+++ ex01/RPN.hpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 17:27:15 by kalmheir          #+#    #+#             */
+/*   Updated: 2023/09/18 18:16:00 by kalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <iostream>
+# include <stdexcept>
+# include <stack>
+# include <sstream>
+# define IS_OPERATOR(c) (c == '+' || c == '-' || c == '*' || c == '/' || c == '%')
+
+typedef enum	e_operator {
+	PLUS = '+',
+	MINUS = '-',
+	MULT = '*',
+	DIV = '/',
+}				t_operator;
+
+typedef struct s_expression {
+	int			left;
+	int			right;
+	t_operator	op;
+}				t_expression;
+
+class RPN {
+	private:
+		std::stack<int>	*_memory;
+		std::string		_expression;
+		static void		validateChars(std::string &);
+		static int		compute(int, int, t_operator);
+		static int		add(int, int);
+		static int		subtract(int, int);
+		static int		multiply(int, int);
+		static int		divide(int, int);
+	public:
+		RPN		(void);
+		RPN		(std::string &);
+		RPN		(const char *);
+		RPN		(RPN const &);
+		~RPN	(void);
+		RPN		&operator=(RPN const &);
+		
+		int			compute(void) const;
+		void		setExpression(std::string &);
+		void		setExpression(const char *);
+		std::string	getExpression(void) const;
+		
+		class InvalidExpressionException : public std::exception {
+			private:
+				std::string	_error;
+			public:
+				InvalidExpressionException	(void);
+				InvalidExpressionException	(std::string);
+				InvalidExpressionException	(const InvalidExpressionException &);
+				~InvalidExpressionException	(void) throw();
+				InvalidExpressionException	&operator=(const InvalidExpressionException &);
+				virtual const char			*what(void) const throw();
+		};
+};
+
+std::ostream	&operator<<(std::ostream &, const RPN &);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 17:26:54 by kalmheir          #+#    #+#             */
+/*   Updated: 2023/09/18 17:33:35 by kalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+int	main(int argc, char *argv[]) {
+	if (argc == 2) {
+		try {
+			RPN expression(argv[1]);
+			std::cout << expression << std::endl;
+		} catch (RPN::InvalidExpressionException &error) {
+			std::cout << "Error:" << error.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
