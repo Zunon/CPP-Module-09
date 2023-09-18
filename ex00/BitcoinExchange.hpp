310ced61f695292ce759cb00f62da3c18fa78e0c
Khalifa AlMheiri <zunon@zunon.io>
2023-09-18 14:59:03 +0400

    feat(ex00): define BitcoinExchange class feat(ex00): add database provided by subject

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,13 +6,31 @@
 /*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 14:31:09 by kalmheir          #+#    #+#             */
-/*   Updated: 2023/09/18 14:36:16 by kalmheir         ###   ########.fr       */
+/*   Updated: 2023/09/18 14:58:03 by kalmheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <map>
+# include <iostream>
+# include <string>
+# include <fstream>
 
+class BitcoinExchange {
+    private:
+		std::map<std::string, double>			*_rates;
+		static std::map<std::string, double>	*parseCSV(std::ifstream &);
+    public:
+        BitcoinExchange				(void);
+		BitcoinExchange				(std::string);
+		BitcoinExchange				(std::ifstream &);
+        BitcoinExchange				(const BitcoinExchange &);
+        ~BitcoinExchange			(void);
+        BitcoinExchange &operator=	(const BitcoinExchange &);
+		double						lookup(std::string);
+		bool						verifyDate(std::string);
+};
 
 #endif
